cc739629e6302aa6dcf1cad3219bf62308c69b60
nanaelie <elienana92@gmail.com>
2025-08-05 00:38:34 +0000

    feat: update home screen logic and torch behavior

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,157 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { useState, useEffect, useRef } from 'react';
+import { StyleSheet, ScrollView, Text, View, TouchableOpacity } from 'react-native';
+import { Camera, CameraView } from 'expo-camera';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+	const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+	const [torchOn, setTorchOn] = useState(false);
+	const [isSosActive, setIsSosActive] = useState(false);
+	const camRef = useRef(null);
+	const intervalRef = useRef<NodeJS.Timer | null>(null);
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+	useEffect(() => {
+		(async () => {
+			const { status } = await Camera.requestCameraPermissionsAsync();
+			setHasPermission(status === 'granted');
+		})();
+	}, []);
+
+	const toggleTorch = () => {
+		if (isSosActive) {
+		  setIsSosActive(false);
+		  if (intervalRef.current) {
+		    clearInterval(intervalRef.current);
+		    intervalRef.current = null;
+		  }
+		}
+
+		setTorchOn((prev) => !prev);
+	};
+
+	const sos = () => {
+	  setIsSosActive((prev) => {
+		const newState = !prev;
+
+		if (newState) {
+		  intervalRef.current = setInterval(() => {
+		    setTorchOn((prev) => !prev);
+		  }, 500);
+		} else {
+		  if (intervalRef.current) {
+		    clearInterval(intervalRef.current);
+		    intervalRef.current = null;
+		  }
+		  setTorchOn(false);
+		}
+
+		return newState;
+	  });
+	};
+
+	if (hasPermission === null) return null;
+	if (hasPermission === false) return <View><Text>No camera permission</Text></View>;
+
+	return (
+		<ScrollView
+			contentContainerStyle={{
+				flex: 1,
+				alignItems: 'center',
+				padding: 32,
+				backgroundColor: '#181818',
+				paddingTop: 100,
+			}}>
+			<CameraView
+				style={{
+					display: 'none',
+				}}
+				ref={camRef}
+				type='back'
+				enableTorch={torchOn}
+			></CameraView>
+			<View style={{
+				flexDirection: 'row',
+				alignItems: 'center',
+				gap: 8,
+			}}>
+			<Text style={{
+				color: '#fff', 
+				fontSize: 32, 
+				fontWeight: 900
+			}} >Torch</Text>
+			</View>
+			<View style={{
+				marginBottom: 60,
+				marginTop: 130,
+				display: 'flex',
+				gap: 15,
+				alignItems: 'center',
+				justifyContent: 'center',
+				}}>
+				<Text style={{
+					color: '#fff', 
+					fontSize: 28, 
+					fontWeight: 700
+					}}>{torchOn ? 'ON' : 'OFF'}</Text>
+
+				<TouchableOpacity style={{
+					width: 150,
+					height: 150,
+					display: 'flex',
+					borderWidth: 5,
+					borderRadius: 999,
+					padding: 15,
+					borderColor: '#ff4500',
+					alignItems: 'center',
+					justifyContent: 'center',
+					}} onPress={toggleTorch}>
+					{
+						torchOn ? (
+							<Image style={{
+							width: '100%',
+							height: '100%',
+							resizeMode: 'contain',
+							}} source={ require('@/assets/images/torch-on.png') } />
+						) : (
+							<Image style={{
+							width: '100%',
+							height: '100%',
+							resizeMode: 'contain',
+							}} source={ require('@/assets/images/torch-off.png') } />
+						)
+					}
+				</TouchableOpacity>
+			</View>
+			<View style={{}}>
+				<TouchableOpacity
+					style={[
+					  {
+						width: 150,
+						display: 'flex',
+						borderRadius: 999,
+						padding: 15,
+						alignItems: 'center',
+						justifyContent: 'center',
+						backgroundColor: '#202020',
+					  },
+					  isSosActive && {
+						shadowColor: '#eee',
+						shadowOffset: { width: 0, height: 2 },
+						shadowOpacity: 0.5,
+						shadowRadius: 4,
+						elevation: 5,
+					  }
+					]}
+
+
+						onPress={sos}>
+					<Text style={{
+					color: '#fff', 
+					fontSize: 16, 
+					fontWeight: 700
+					}}>SOS</Text>
+				</TouchableOpacity>
+			</View>
+		</ScrollView>
+	);
+}
